fc1ba311f737d6e0772a489f62232de0d6bf9d4b
Gáspár Zoltán <gaspar.zoltan@bad.hu>
2020-04-27 22:42:13 +0200

    secret_key

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('QqPC(6!jtRvV73/cw]59+zFE]y\X\-nBfx#@m+"!P6:;p)h?w{6CFEGA;)}ougss')
+SECRET_KEY = config('nTrkufKT4TPeK6UMTDuobPcvph7KaFmPKzgcevp5A7Sku3RJoTbuKUVN7ggNWyMS')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
